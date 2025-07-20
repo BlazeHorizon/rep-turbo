de1bc91077c9954397d5f8c96ac2411a234d8af6
Blaze Horizon <blazenewhorizon@gmail.com>
2025-07-20 13:32:54 -0400

    Version 1.2 - Touch up the credits more.

--- start_menu.gd
+++ start_menu.gd
@@ -18,3 +18,9 @@ func _ready() -> void:
 
 func _on_credits_button_pressed() -> void:
 	credits_panel.visible = not credits_panel.visible
+	credits_panel.get_child(1).grab_focus()
+
+
+func _on_credits_close_button_pressed() -> void:
+	credits_panel.visible = false
+	credits_button.grab_focus()
